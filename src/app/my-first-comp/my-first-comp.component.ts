4f5bc2d1a8de2a6929f20ab75f7428970b92ac75
chadibenrejeb <rejebchadi@gmail.com>
2024-06-23 01:44:54 +0100

    initial commit

--- src/app/my-first-comp/my-first-comp.component.ts
+++ src/app/my-first-comp/my-first-comp.component.ts
@@ -1,19 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, AfterViewInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-my-first-comp',
   templateUrl: './my-first-comp.component.html',
   styleUrls: ['./my-first-comp.component.scss']
 })
-export class MyFirstCompComponent {
+export class MyFirstCompComponent implements AfterViewInit {
   name: string = '';
   email: string = '';
   message: string = '';
+  isSubmitted: boolean = false;
+
+  nameInput: HTMLInputElement | null = null;
+  emailInput: HTMLInputElement | null = null;
+  messageInput: HTMLTextAreaElement | null = null;
+  messages : Array<any> = [] ;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.nameInput = document.getElementById('name') as HTMLInputElement;
+      this.emailInput = document.getElementById('email') as HTMLInputElement;
+      this.messageInput = document.getElementById('message') as HTMLTextAreaElement;
+    }
+  }
+
 
   onSubmit() {
-    // Handle form submission logic here if needed
     console.log('Name:', this.name);
     console.log('Email:', this.email);
     console.log('Message:', this.message);
+    this.isSubmitted = true;
+    this.messages.push({
+      'name' : this.name , 
+      'email': this.email,
+      'message' : this.message
+    });
+    console.log(this.messages);
+     
+
+    // if (this.nameInput) {
+    //   this.nameInput.value = '';
+    // }
+    // if (this.emailInput) {
+    //   this.emailInput.value = '';
+    // }
+    // if (this.messageInput) {
+    //   this.messageInput.value = '';
+    //}
   }
 }
